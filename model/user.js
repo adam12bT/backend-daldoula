13eb3bd9eabaabf7c97bc7f3d9b916b717bb083b
adam <alidenis970@gmail.com>
2025-05-08 19:07:47 +0100

    first cozzzzz

--- model/user.js
+++ model/user.js
@@ -11,6 +11,7 @@ const UserSchema = new mongoose.Schema({
   skills: { type: [String] },
   rating: { type: Number, min: 1, max: 5 },
   bio: { type: String, },
+  image: { type: String }, 
 
   reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }]
 }, {
